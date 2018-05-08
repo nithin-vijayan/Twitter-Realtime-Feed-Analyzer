45636ecbf70f175695eda3f5ce8fe763fe1dbb6f
Nithin K <vijayan@apple.com>
2018-05-08 18:53:35 +0530

    Added helper methods for generating reports

---
+++ reportHelper.py
@@ -0,0 +1,55 @@
+import config
+import operator
+from tweetHelper import getActiveTweets, getTweetsByUser, getUrls, getDomainOccurance, getWordOccurance
+
+def generateUserReport(activeTweets):
+	userReport = []
+	for user,tweets in getTweetsByUser(activeTweets).iteritems():
+		userTweet = "%s posted %s Tweet(s)" % (user, len(tweets))
+		userReport.append(userTweet)
+	return userReport
+
+def generateLinksReport(activeTweets):
+	domainReport = []
+	urlsConsolidated = getUrls(activeTweets)
+	domainOccurance = getDomainOccurance(urlsConsolidated)
+	totalUrls = len(urlsConsolidated)
+	sortedDomainList = sorted(domainOccurance.items(), key=operator.itemgetter(1), reverse=True)
+	for each in sortedDomainList:
+		domainInfo = "%s - %s time(s)"%(each[0], each[1])
+		domainReport.append(domainInfo)
+	return totalUrls, domainReport
+
+def generateContentReport(activeTweets):
+	wordReport = []
+	wordList = getWordOccurance(activeTweets)
+	totalWords = len(wordList)
+	sortedwordList = sorted(wordList.items(), key=operator.itemgetter(1), reverse=True)
+	for each in sortedwordList[:10]:
+		wordInfo = "%s - %s time(s)"%(each[0], each[1])
+		wordReport.append(wordInfo)
+	return totalWords, wordReport
+
+def printReport(streamBuffer):
+	activeTweets = getActiveTweets(streamBuffer, config.expiryMinutes)
+	#Char sequence to clear screen
+	print(chr(27) + "[2J")
+	print """
+		User Report
+	"""
+	userReport = generateUserReport(activeTweets)
+	print "\n".join(userReport)
+
+	print """
+		Links Report
+	"""
+	totalUrls, domainReport = generateLinksReport(activeTweets)
+	print "Total Urls : %s" % (totalUrls)
+	print "\n".join(domainReport)
+
+	print """
+		Content Report
+	"""
+	totalWords, wordReport = generateContentReport(activeTweets)
+	print "Total Words : %s" % (totalWords)
+	print "\n".join(wordReport)
